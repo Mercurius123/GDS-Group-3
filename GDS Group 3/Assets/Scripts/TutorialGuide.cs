b6d0568e1b7cf3a09adff140eba4d5d31369041a
Mercurius Awad <1015510@student.sae.edu.au>
2020-06-23 15:51:19 +1000

    A tutorial level / in game guidance (prototype)
    still a prototype

---
+++ GDS Group 3/Assets/Scripts/TutorialGuide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialGuide : MonoBehaviour
+{
+    public GameObject blockText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        blockText.SetActive(true);
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        blockText.SetActive(false);
+    }
+
+
+}
