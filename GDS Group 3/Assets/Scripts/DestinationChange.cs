ef2164741a2a238267c7431fe7870dcb2213d40d
Mercurius Awad <1015510@student.sae.edu.au>
2020-06-24 11:48:25 +1000

    AI moving to different to different positions
    somewhat pog

---
+++ GDS Group 3/Assets/Scripts/DestinationChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationChange : MonoBehaviour
+{
+    public int xPos;
+    public int zPos;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Character")
+       {
+            xPos = Random.Range(11, -11);
+            zPos = Random.Range(3, -10);
+            this.gameObject.transform.position = new Vector3(xPos, 1.5f, zPos);
+
+       } 
+    }
+
+
+
+}
