5c014beece7d26afd908c0041cae2d0b7de36eda
Luke Van Lierop <spartan901@hotmail.com>
2020-06-29 22:46:15 +1000

    UI bubble noise, moving buttons, separate start scene. pause menu issue

---
+++ GDS Group 3/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    [SerializeField] GameObject playerController;
+    [SerializeField] GameObject background;
+    [SerializeField] GameObject craftMenu;
+    [SerializeField] GameObject charMenu;
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject saveMenu;
+    [SerializeField] GameObject loadMenu;
+    [SerializeField] GameObject encycloMenu;
+    [SerializeField] GameObject controlsMenu;
+    bool paused;
+
+     
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                background.SetActive(false);
+                craftMenu.SetActive(false);
+                charMenu.SetActive(false);
+                pauseMenu.SetActive(false);
+                saveMenu.SetActive(false);
+                loadMenu.SetActive(false);
+                encycloMenu.SetActive(false);
+                controlsMenu.SetActive(false);
+                ResumeGame();
+            }
+            else
+            {
+                background.SetActive(true);
+                pauseMenu.SetActive(true);                
+                PauseGame();
+            } 
+        }
+
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            charMenu.SetActive(true);
+            background.SetActive(true);
+            PauseGame();
+        }
+
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            craftMenu.SetActive(true);
+            background.SetActive(true);
+            PauseGame();
+        }
+    }
+        
+    public void PauseGame()
+    {
+        playerController.GetComponent<FirstPersonAIO>().ControllerPause();
+        paused = true;
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        playerController.GetComponent<FirstPersonAIO>().ControllerPause();
+        paused = false;
+        Time.timeScale = 1;
+    }
+    public void Exit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("MAIN");
+    }
+
+    public void LeaveGame()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
