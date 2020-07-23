673d2f05a503192c49a8c7a497ada426b594cfb4
Mercurius Awad <1015510@student.sae.edu.au>
2020-07-23 20:17:16 +1000

    AI Improved, Flocking Fish Implemented
    not bad but still needs lots of work done :/

---
+++ GDS Group 3/Assets/Scripts/EnemyAiNavMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAiNavMesh : MonoBehaviour
+{
+
+    private NavMeshAgent Fish;
+
+    public GameObject Player;
+
+    public float FishDistanceRun = 4.0f;
+
+
+
+
+
+    void Start()
+    {
+        Fish = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+
+        if (distance < FishDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            Fish.SetDestination(newPos);
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ GDS Group 3/Assets/Scripts/WayPointAi.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointAi : MonoBehaviour
+{
+
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.1f;
+    private int LastWaypointIndex;
+
+    private float movementSpeed = 3.0f;
+    
+    void Start()
+    {
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+    
+    void Update()
+    {
+        float movementStep = movementSpeed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+    }
+}
