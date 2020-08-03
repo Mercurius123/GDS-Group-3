81e7d3f01d39c29b4d11b9702979c50a9e60bb43
Mercurius Awad <1015510@student.sae.edu.au>
2020-08-03 19:17:10 +1000

    Pick up & Sound on collision added back
    nice

---
+++ GDS Group 3/Assets/Scripts/MainPlayerFunctions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainPlayerFunctions : MonoBehaviour
+{
+    
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+    
+
+    
+
+
+    public AudioSource tickSource;
+
+
+    
+    void Start()
+    {
+        
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winText.text = "";
+
+        tickSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        
+       
+
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+            tickSource.Play ();
+        }
+
+      
+
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            SceneManager.LoadScene("End");
+            Cursor.visible = true;
+        }
+
+    }
+}
