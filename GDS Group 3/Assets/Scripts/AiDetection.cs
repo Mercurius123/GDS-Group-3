eb5ef29866b40aa6dbca41f055530e6793b32e42
Mercurius Awad <1015510@student.sae.edu.au>
2020-08-18 15:11:50 +1000

    AI improved
    Predators can now track you anywhere as long as you are within their radius detection!

---
+++ GDS Group 3/Assets/Scripts/AiDetection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiDetection : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float speed;
+    public float FishDistanceRun = 4.0f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+
+        if (distance < FishDistanceRun)
+        {
+            transform.LookAt(Player.transform.position);
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+
+
+    }
+
+    //void OnCollisionStay(Collision obj)
+    //{
+
+    //    if(obj.gameObject.name == "Player")
+    //    {
+    //        speed = 0f;
+    //    }
+    //    else{
+
+    //        speed = 5;
+    //    }
+
+
+
+
+
+    //}
+}
