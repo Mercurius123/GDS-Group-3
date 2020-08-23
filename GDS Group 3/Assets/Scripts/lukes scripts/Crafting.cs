d5d39455c6d4a462b410c440397ad0dc0a82b12b
Luke <spartan901@hotmail.com>
2020-08-24 01:39:56 +1000

    crafting properly implemented
    still alot to do

---
+++ GDS Group 3/Assets/Scripts/lukes scripts/Crafting.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crafting : MonoBehaviour
+{
+    public GameObject Interactable;
+    public int oxyTankCost = 10;
+    public int breathAmountAdded; //check if overlord uses a float or a int
+    public int TPGunCost = 10;
+    public int healthPackCost = 10;
+    public int healthPackCurrent = 0;
+    public int healthPackMax = 5;
+    public int healthPackMin = 0;
+    public float healAmount; //check if overloard uses floats of an int
+    public Text TPGunCostText;
+    public GameObject gunButton;
+    public Text oxyTankCostText;
+    public GameObject oxyTankButton;
+    public Text healthPackCostText;
+    public GameObject healthButton;
+    public bool oxyTankCrafted = false;
+    public bool upgradedOxyTank = false;
+    public bool deactivate = false;
+    public bool TPGCrafted = false;
+    public float matterTimer = 3;
+    public float maxmatterTimer = 3;
+    public Text craftingNotifications;
+    public bool craftingNote = false;
+
+    private void Start()
+    {
+        TPGunCostText.text = ("Cost: " + TPGunCost);
+        oxyTankCostText.text = ("Cost: " + oxyTankCost);
+        healthPackCostText.text = ("Cost: " + healthPackCost);
+    }
+    private void Update()
+    {
+        if (craftingNote == true)
+        {
+            craftingNotifications.gameObject.SetActive(true);
+            matterTimer -= Time.unscaledDeltaTime;
+            if (matterTimer <= 0)
+            {
+                craftingNotifications.text = "";
+                craftingNotifications.gameObject.SetActive(false);
+                craftingNote = false;
+            }
+        }
+    }
+
+    public void CraftOxyTank()
+    {
+        if (oxyTankCrafted == true)
+        {
+            if (oxyTankCost <= Interactable.GetComponent<Interactable>().Matter)
+            {
+                matterTimer = maxmatterTimer;
+                Interactable.GetComponent<Interactable>().Matter = Interactable.GetComponent<Interactable>().Matter - oxyTankCost;
+                oxyTankCost = 0;
+                upgradedOxyTank = true;
+                oxyTankButton.SetActive(false);
+                oxyTankCostText.gameObject.SetActive(false);
+                craftingNotifications.text = "You Crafted the Double Oxygen Tank!";
+                craftingNote = true;
+                //still have to change the breath stats in here
+                //also change the inventory stuff
+                //also activate the gameobject
+            }
+            else
+            {
+                matterTimer = maxmatterTimer;
+                craftingNotifications.text = "You need more Matter to Build that.";
+                craftingNote = true;
+            }
+        }
+        else
+        {
+            if (oxyTankCost <= Interactable.GetComponent<Interactable>().Matter)
+            {
+                matterTimer = maxmatterTimer;
+                Interactable.GetComponent<Interactable>().Matter = Interactable.GetComponent<Interactable>().Matter - oxyTankCost;
+                Interactable.GetComponent<Interactable>().MatterCountText.text = ("Matter: " + Interactable.GetComponent<Interactable>().Matter);
+                oxyTankCost = 20;
+                oxyTankCrafted = true;
+                oxyTankCostText.text = "Cost: " + oxyTankCost;                
+                craftingNotifications.text = "You Crafted An Oxygen Tank!";
+                craftingNote = true;
+                //still have to change the breath stats in here
+                //also change the inventory stuff
+                //also activate the gameobject
+            }
+            else
+            {
+                matterTimer = maxmatterTimer;
+                craftingNotifications.text = "You need more Matter to Build that.";
+                craftingNote = true;
+            }
+
+        }
+
+    }
+    public void CraftHealth()
+    {
+        if (healthPackCurrent == healthPackMax)
+        {
+            matterTimer = maxmatterTimer;
+            craftingNotifications.text = "You Have Max Health Items";
+            craftingNote = true;
+        }
+        else
+        {
+            if (healthPackCost <= Interactable.GetComponent<Interactable>().Matter)
+            {
+                matterTimer = maxmatterTimer;
+                Interactable.GetComponent<Interactable>().Matter = Interactable.GetComponent<Interactable>().Matter - healthPackCost;
+                Interactable.GetComponent<Interactable>().MatterCountText.text = ("Matter: " + Interactable.GetComponent<Interactable>().Matter);
+                healthPackCurrent = healthPackCurrent + 1;
+                craftingNotifications.text = "You Crafted a Health Stim!";
+                craftingNote = true;
+                //change the inventory stuff
+            }
+            else
+            {
+                matterTimer = maxmatterTimer;
+                craftingNotifications.text = "You need more Matter to Build that.";
+                craftingNote = true;
+            }
+        }
+    }
+    public void TPGCraft()
+    {
+        if (TPGunCost <= Interactable.GetComponent<Interactable>().Matter)
+        {
+            matterTimer = maxmatterTimer;
+            Interactable.GetComponent<Interactable>().Matter = Interactable.GetComponent<Interactable>().Matter - TPGunCost;
+            Interactable.GetComponent<Interactable>().MatterCountText.text = ("Matter: " + Interactable.GetComponent<Interactable>().Matter);
+            TPGunCost = 0;
+            TPGCrafted = true;
+            gunButton.SetActive(false);
+            TPGunCostText.gameObject.SetActive(false);
+            craftingNotifications.text = "You Crafted the Matter Teleporter!";
+            craftingNote = true;
+            //change the inventory stuff
+            //check if can be tied to the underwater gun animations
+            //also activate the gameobject
+        }
+        else
+        {
+            matterTimer = maxmatterTimer;
+            craftingNotifications.text = "You need more Matter to Build that.";
+            craftingNote = true;
+        }
+    }
+}
---
+++ GDS Group 3/Assets/Scripts/lukes scripts/Interactable.cs
@@ -0,0 +1,160 @@
+﻿using Opsive.UltimateCharacterController.Demo.Objects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject menuObject;
+    public GameObject collision;
+    public Transform Player;
+    public Text GUIText;
+    public Text GUITextChange;
+    public int changeTextTime;
+    public Text MatterCountText;
+    public int carriedGarbageCount;
+    public int maxGarbageCount;
+    public int Matter;
+    public int smallGarbageValue = 10;
+    public int largeGarbageValue = 50;
+    public bool TPGunEquipped = false;
+    public bool interactionHappened = false;
+    public float GUITimer = 3;
+    public float maxTimer = 3;
+         
+    
+    void Start()
+    {
+       Rigidbody rb = GetComponent<Rigidbody>();
+       rb.isKinematic = false;
+    }
+
+    void Update()
+    {
+        if (interactionHappened == true)
+        {
+            GUIText.gameObject.SetActive(false);
+            GUITextChange.gameObject.SetActive(true);
+            GUITimer -= Time.deltaTime;
+            if (GUITimer <= 0)
+            {
+                GUITextChange.gameObject.SetActive(false);
+                GUITextChange.text = ("");
+                interactionHappened = false;
+            }
+        }
+    }
+
+    public void OnCollisionStay(Collision col)
+    {
+        Debug.Log("Collision with" + col.gameObject.name);
+        collision = col.gameObject;
+        if (interactionHappened == false)
+        {
+            if (col.gameObject.name == "Garbage Storage")
+            {
+                GUIText.text = ("Press F to Deposit Garbage");
+                GUIText.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    GarbageStorageUI();
+                }
+            }
+            else if (col.gameObject.name == "CraftingBench")
+            {
+                GUIText.text = ("Press F to Craft Something");
+                GUIText.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    CraftingBenchUI();
+                }
+            }
+            else if (col.gameObject.tag == "LargeGarbage")
+            {
+                GUIText.text = ("Press F to Collect");
+                GUIText.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    LargeGarbageUI();
+                }
+            }
+            else if (col.gameObject.tag == "SmallGarbage")
+            {
+                GUIText.text = ("Press F to Collect");
+                GUIText.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    SmallGarbageUI();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("displaying through update");
+        }
+    }
+
+    public void OnCollisionExit(Collision col)
+    {
+        GUIText.text = ("");
+        GUIText.gameObject.SetActive(false);
+        GUITextChange.text = ("");
+        GUITextChange.gameObject.SetActive(false);
+        interactionHappened = false;
+    }
+        
+    public void GarbageStorageUI()
+    {
+        if (carriedGarbageCount == 0)
+        {
+            GUITextChange.text = ("You have nothing to deposit");
+            GUITimer = maxTimer;
+            interactionHappened = true;
+        }
+        else
+        {
+            Matter = Matter + (carriedGarbageCount * smallGarbageValue);
+            carriedGarbageCount = 0;
+            MatterCountText.text = ("Matter Count: " + Matter);
+            GUITextChange.text = ("You have " + Matter + " total.");
+            GUITimer = maxTimer;
+            interactionHappened = true;
+        }
+    }
+    public void CraftingBenchUI()
+    {
+        menuObject.GetComponent<MenuControl>().craftMenuActive = true;
+        GUIText.text = ("");
+        GUIText.gameObject.SetActive(false);
+    }
+
+    public void LargeGarbageUI()
+    {
+        if (TPGunEquipped == false)
+        {
+            GUITextChange.text = ("This is too Heavy to carry.");
+            GUITimer = maxTimer;
+            interactionHappened = true;
+        }
+        else
+        {
+            Debug.Log("Teleported");
+        }
+    }
+
+    public void SmallGarbageUI()
+    {
+        if(carriedGarbageCount < maxGarbageCount)
+        {
+            carriedGarbageCount = carriedGarbageCount + 1;
+            Destroy(collision);
+        }
+        else
+        {
+            GUITextChange.text = ("You are carrying to much already!");
+            GUITimer = maxTimer;
+            interactionHappened = true;
+        }        
+    }
+}
