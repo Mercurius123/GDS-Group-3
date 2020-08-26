feabf6a50adf04f37a3f097bc15f07b005f2cdd4
Cooper Braun <1015429@student.sae.edu>
2020-08-26 10:54:18 +1000

    Predator attacks done and linked to player being hurt

---
+++ GDS Group 3/Assets/Scripts/PredatorAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredatorAttack : MonoBehaviour
+{
+    public GameObject hurtUI;
+    public bool touched;
+
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "Predator")
+        {
+            hurtUI.SetActive(true);
+            Invoke("TurnOffHurtUI", 1f);
+            //put sound effect here
+            gameObject.GetComponent<PlayerOverlord>().PlayerHealth = gameObject.GetComponent<PlayerOverlord>().PlayerHealth - 50;
+
+            touched = true;
+        }
+    }
+
+    void TurnOffHurtUI()
+    {
+        hurtUI.SetActive(false);
+    }
+}
