962f462b3e3fe60c32f8a764433f5afbd01e4957
Arjun <1016487@student.sae.edu.au>
2020-08-28 22:59:38 +1000

    Saving Before Build

---
+++ GDS Group 3/Assets/Scripts/Flora.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flora : MonoBehaviour
+{
+    public GameObject Corals;
+    public GameObject Seaweed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Corals.SetActive(false);
+        Seaweed.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void macroalgae()
+    {
+        Corals.SetActive(false);
+        Seaweed.SetActive(true);
+    }
+    public void Anthozoa()
+    {
+        Seaweed.SetActive(false);
+        Corals.SetActive(true);
+    }
+}
